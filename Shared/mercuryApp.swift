5159a1c75d89dc2e4c3f5bcda6195b40f4cd41e6
KarKar100 <morkiral@gmail.com>
2021-06-19 14:49:29 +0300

    Initial project version

---
+++ Shared/mercuryApp.swift
@@ -0,0 +1,17 @@
+//
+//  mercuryApp.swift
+//  Shared
+//
+//  Created by Diana on 19.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct mercuryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
